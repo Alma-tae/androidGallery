ce162676ce8afd3b0c9c56b74b975717d86943be
Alma-tae <taehuikim0430@gmail.com>
2021-04-08 23:22:37 +0900

    Galley practice

---
+++ app/src/main/java/naver/peekaboo21/handwritten/ImageDisplay.java
@@ -0,0 +1,14 @@
+package naver.peekaboo21.handwritten;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+    }
+}
